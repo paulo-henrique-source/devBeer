90e430cdbf380f7a14be6d95309399e6563c4586
Henrique Tavares <ihenrits@gmail.com>
2021-06-25 20:05:04 -0300

    landing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
 import { Room } from '../pages/Room';
+import { AdminRoom } from '../pages/AdminRoom';
 
 export default function Routes() {
   return (
@@ -10,7 +11,9 @@ export default function Routes() {
        <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id+" component={Room}  />
+          <Route path="/rooms/:id+" component={Room}  /
+          >
+          <Route path="/admin/rooms/:id+" component={AdminRoom}  />
        </Switch>
     </BrowserRouter>
   )
